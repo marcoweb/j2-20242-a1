75da7ab21270d7d91ef935b4787f9e9f5f3752c4
marcoweb <marcocarvalho.web@gmail.com>
2024-08-13 19:51:44 +0000

    Aula 13/08/2024

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -4,22 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.model.Aluno;
 import application.repository.AlunoRepository;
 
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
     @Autowired
     private AlunoRepository alunoRepo;
 
-    @GetMapping("/alunos")
+    @GetMapping
     public Iterable<Aluno> list() {
         return alunoRepo.findAll();
     }
 
-    @PostMapping("/alunos")
+    @PostMapping
     public Aluno insert(@RequestBody Aluno aluno) {
         return alunoRepo.save(aluno);
     }
