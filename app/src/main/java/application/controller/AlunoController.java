7cde3dd79d5d0b785487cdb2ac920a56e41163ff
marcoweb <marcocarvalho.web@gmail.com>
2024-08-19 17:43:38 +0000

    Aula 19/08/2024

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import application.model.Aluno;
 import application.repository.AlunoRepository;
@@ -30,5 +34,21 @@ public class AlunoController {
     @GetMapping("/{id}")
     public Aluno details(@PathVariable long id) {
         return alunoRepo.findById(id).get();
+    }
+
+    @PutMapping("/{id}")
+    public Aluno put(
+        @PathVariable long id,
+        @RequestBody Aluno novosDados) {
+        Optional<Aluno> resultado = alunoRepo.findById(id);
+
+        resultado.get().setNome(novosDados.getNome());
+
+        return alunoRepo.save(resultado.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        alunoRepo.deleteById(id);
     }
 }
